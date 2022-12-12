09047fc15467daa0a6b784c113cd461c88eec1db
Manuel Calle <callemac88@gmail.com>
2022-12-12 11:36:58 -0500

    auth and chart.js

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,27 @@
-const people = [
-  {
-    name: "Jane Cooper",
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
-    email: "jane.cooper@example.com",
-    image:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-];
+import endPoints from "@services/api";
+import useFetch from "@hooks/useFetch";
+import Link from "next/link";
+
+interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: {
+    id: number;
+    name: string;
+  };
+  images: string[];
+}
+
+const PRODUCT_LIMIT = 15;
+const PRODUCT_OFFSET = 15;
 
 export default function Dashboard() {
+  const products = useFetch(
+    endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)
+  );
+  console.log(products);
   return (
     <>
       <div className="flex flex-col">
@@ -30,70 +41,75 @@ export default function Dashboard() {
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Title
+                      Category
                     </th>
                     <th
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Status
+                      Price
                     </th>
                     <th
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Role
+                      Id
                     </th>
                     <th scope="col" className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Delete</span>
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
-                    <tr key={person.email}>
+                  {products.map((product: IProduct) => (
+                    <tr key={`Product-item-${product.id}`}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
                             <img
                               className="h-10 w-10 rounded-full"
-                              src={person.image}
+                              src={product.images[0]}
                               alt=""
+                              width={100}
+                              height={100}
                             />
-                          </div>
-                          <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">
-                              {person.name}
-                            </div>
-                            <div className="text-sm text-gray-500">
-                              {person.email}
-                            </div>
                           </div>
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="text-sm text-gray-900">
-                          {person.title}
+                          {product.category.name}
                         </div>
                         <div className="text-sm text-gray-500">
-                          {person.department}
+                          {product.category.id}
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          Active
+                          {product.price}
                         </span>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.role}
+                        {product.id}
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a
+                        <Link
                           href="/login"
                           className="text-indigo-600 hover:text-indigo-900"
                         >
                           Edit
-                        </a>
+                        </Link>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <Link
+                          href="/login"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Delete
+                        </Link>
                       </td>
                     </tr>
                   ))}
